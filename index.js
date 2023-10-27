de3ee6509953901c7b4010c150dd0d9d57fabd94
karam2620 <karambirsingh739@gmail.com>
2023-10-27 13:57:22 +0530

    project java

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-var a=10
-var b=20
-var c=a+b
-alert("show my addition result " +c)
+// var a=10
+// var b=20
+// var c=a+b
+// alert("show my addition result " +c)
+
+// var a=20
+// var b=40
+// var c=a-b
+// alert(`my substraction is ${c}`)
+
+var x=200
+x=150
+x=90
+var y=300
+y=250
+var z=x+y
+console.log(z)
