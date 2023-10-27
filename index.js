515ed70c758ae8e1c176c6cd5c6d3972318670ca
karam2620 <karambirsingh739@gmail.com>
2023-10-27 14:21:21 +0530

    second var method

--- index.js
+++ index.js
@@ -8,10 +8,18 @@
 // var c=a-b
 // alert(`my substraction is ${c}`)
 
-var x=200
-x=150
-x=90
-var y=300
-y=250
-var z=x+y
-console.log(z)
+// var x=200
+// x=150
+// x=90
+// var y=300
+// y=250
+// var z=x+y
+// console.log(z)
+
+// let method
+// second var method
+var x=prompt("enter your first number")
+var y=prompt("enter your second number")
+var z=Number(x)+Number(y)
+// console.log(z)
+alert(" your first number is " + x + " your second number is " + y + " your total is " + z )
